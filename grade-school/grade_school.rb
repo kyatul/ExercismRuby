6d8fba588be60777fd21d2d33a5187dd579b8cb1
kyatul <atul.21j@gmail.com>
2016-03-23 11:24:12 +0530

    exercism grade-school 2nd iteration

--- grade-school/grade_school.rb
+++ grade-school/grade_school.rb
@@ -1,30 +1,29 @@
 class School
-  VERSION = 1	
+  VERSION = 1
   def initialize
-  	@student_records = {}
-  end
-  
-  def add name, grade
-    if @student_records.key?(grade)
-      students = @student_records[grade]
-      students = students.push name
-      students = students.sort
-      @student_records[grade] = students 
-    else
-      @student_records[grade] = [name]	
-    end
-    
-  end	
-  
-  def to_h
-  	return Hash[@student_records.sort]
+    @student_records = {}
   end
 
-  def grade student_grade
-  	if @student_records.key?(student_grade)
+  def add(name, grade)
+    if @student_records.key?(grade)
+      students = @student_records[grade]
+      students = students.push(name)
+      students = students.sort
+      @student_records[grade] = students
+    else
+      @student_records[grade] = [name]
+    end
+  end
+
+  def to_h
+    Hash[@student_records.sort]
+  end
+
+  def grade(student_grade)
+    if @student_records.key?(student_grade)
       @student_records[student_grade]
     else
       []
-    end    
-  end	
-end	
+    end
+  end
+end
